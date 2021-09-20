a9b802c837efb8899f00bae1daf3076e59c23b01
ckeller22 <chriskeller222@gmail.com>
2021-09-20 15:21:33 -0700

    finished builders

--- ShoppingCartServiceTests/builders/ItemBuilder.cs
+++ ShoppingCartServiceTests/builders/ItemBuilder.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ShoppingCartService.DataAccess.Entities;
+
 
 namespace ShoppingCartServiceTests.builders
 {
-    internal class ItemBuilder
+    public class ItemBuilder
     {
+        private string _productId = "prod1";
+        private string _productName = "product1";
+        private double _price = 10.0;
+        private uint _quantity = 1;
+
+        public Item Build() {  return new Item() { ProductId = _productId, ProductName = _productName, Price = _price, Quantity = _quantity}; }
+
+        public ItemBuilder WithProductId(string productId)
+        {
+            this._productId = productId;
+            return this;
+        }
+
+        public ItemBuilder WithProductName(string productName)
+        {
+            this._productName = productName;
+            return this;
+        }
+
+        public ItemBuilder WithPrice(double price)
+        {
+            this._price = price;
+            return this;
+        }
+
+        public ItemBuilder WithQuantity(uint quantity)
+        {
+            this._quantity = quantity;
+            return this;
+        }
     }
 }
